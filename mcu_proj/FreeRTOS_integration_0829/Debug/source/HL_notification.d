e0c25b6f5e8b3016935c8329b3358c98c95db7ef
ahnsangjae <sangjae2015@naver.com>
2018-08-29 14:20:13 +0900

    MCU PROJ integration

---
+++ mcu_proj/FreeRTOS_integration_0829/Debug/source/HL_notification.d
@@ -0,0 +1,65 @@
+# FIXED
+
+source/HL_notification.obj: ../source/HL_notification.c
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_esm.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_esm.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sys_common.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_hal_stdtypes.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+source/HL_notification.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_adc.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_adc.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_can.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_can.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_gio.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_gio.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sci.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_sci.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_i2c.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_i2c.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_etpwm.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_etpwm.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_eqep.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_eqep.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_epc.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_epc.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sys_dma.h
+source/HL_notification.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_dma.h
+
+../source/HL_notification.c: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_esm.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_esm.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sys_common.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_adc.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_adc.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_can.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_can.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_gio.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_gio.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sci.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_sci.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_i2c.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_i2c.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_etpwm.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_etpwm.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_eqep.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_eqep.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_epc.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_epc.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_sys_dma.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/HL_reg_dma.h: 
---
+++ mcu_proj/FreeRTOS_integration_0829/Debug/source/os_croutine.d
@@ -0,0 +1,41 @@
+# FIXED
+
+source/os_croutine.obj: ../source/os_croutine.c
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/FreeRTOS.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stddef.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+source/os_croutine.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/FreeRTOSConfig.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_projdefs.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_portable.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_portmacro.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_mpu_wrappers.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_task.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_list.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_croutine.h
+source/os_croutine.obj: C:/Users/Owner/workspace_v8/FreeRTOS/include/os_list.h
+
+../source/os_croutine.c: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/FreeRTOS.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stddef.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/FreeRTOSConfig.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_projdefs.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_portable.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_portmacro.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_mpu_wrappers.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_task.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_list.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_croutine.h: 
+C:/Users/Owner/workspace_v8/FreeRTOS/include/os_list.h: 
