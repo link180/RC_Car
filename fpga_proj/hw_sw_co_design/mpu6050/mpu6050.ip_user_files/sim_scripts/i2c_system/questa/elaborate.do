89c43d983f4552f8c41211ddeb7ddf3ae9f06d75
silenc3502 <gcccompil3r@gmail.com>
2018-09-02 23:11:39 +0900

    FPGA Summation Prepare

---
+++ fpga_proj/hw_sw_co_design/mpu6050/mpu6050.ip_user_files/sim_scripts/i2c_system/questa/elaborate.do
@@ -0,0 +1,1 @@
+vopt -64 +acc -l elaborate.log -L secureip -L xil_defaultlib -work xil_defaultlib xil_defaultlib.i2c_system -o i2c_system_opt
---
+++ fpga_proj/hw_sw_co_design/mpu6050/mpu6050.srcs/sources_1/bd/i2c_system/hdl/i2c_system_wrapper.vhd
@@ -0,0 +1,133 @@
+--Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2015.4 (lin64) Build 1412921 Wed Nov 18 09:44:32 MST 2015
+--Date        : Tue Jul 31 01:10:28 2018
+--Host        : sdr-Samsung-DeskTop-System running 64-bit Ubuntu 16.04.3 LTS
+--Command     : generate_target i2c_system_wrapper.bd
+--Design      : i2c_system_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity i2c_system_wrapper is
+  port (
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    iic_0_scl_io : inout STD_LOGIC;
+    iic_0_sda_io : inout STD_LOGIC
+  );
+end i2c_system_wrapper;
+
+architecture STRUCTURE of i2c_system_wrapper is
+  component i2c_system is
+  port (
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    IIC_0_sda_i : in STD_LOGIC;
+    IIC_0_sda_o : out STD_LOGIC;
+    IIC_0_sda_t : out STD_LOGIC;
+    IIC_0_scl_i : in STD_LOGIC;
+    IIC_0_scl_o : out STD_LOGIC;
+    IIC_0_scl_t : out STD_LOGIC
+  );
+  end component i2c_system;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal iic_0_scl_i : STD_LOGIC;
+  signal iic_0_scl_o : STD_LOGIC;
+  signal iic_0_scl_t : STD_LOGIC;
+  signal iic_0_sda_i : STD_LOGIC;
+  signal iic_0_sda_o : STD_LOGIC;
+  signal iic_0_sda_t : STD_LOGIC;
+begin
+i2c_system_i: component i2c_system
+     port map (
+      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_cas_n => DDR_cas_n,
+      DDR_ck_n => DDR_ck_n,
+      DDR_ck_p => DDR_ck_p,
+      DDR_cke => DDR_cke,
+      DDR_cs_n => DDR_cs_n,
+      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_odt => DDR_odt,
+      DDR_ras_n => DDR_ras_n,
+      DDR_reset_n => DDR_reset_n,
+      DDR_we_n => DDR_we_n,
+      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      IIC_0_scl_i => iic_0_scl_i,
+      IIC_0_scl_o => iic_0_scl_o,
+      IIC_0_scl_t => iic_0_scl_t,
+      IIC_0_sda_i => iic_0_sda_i,
+      IIC_0_sda_o => iic_0_sda_o,
+      IIC_0_sda_t => iic_0_sda_t
+    );
+iic_0_scl_iobuf: component IOBUF
+     port map (
+      I => iic_0_scl_o,
+      IO => iic_0_scl_io,
+      O => iic_0_scl_i,
+      T => iic_0_scl_t
+    );
+iic_0_sda_iobuf: component IOBUF
+     port map (
+      I => iic_0_sda_o,
+      IO => iic_0_sda_io,
+      O => iic_0_sda_i,
+      T => iic_0_sda_t
+    );
+end STRUCTURE;
